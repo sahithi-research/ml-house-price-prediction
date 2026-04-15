432d6005305b97f1311f9c3d95d93243307c50d8
sahithi-research <sahithipiska24@gmail.com>
2026-04-15 10:50:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import pandas as pd
-import matplotllib.pyplot as plt
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
